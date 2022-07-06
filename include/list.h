897824c3133fce5bb54d6f39f17a8ee816ec3f24
Kqramazov <yurisvarozhich@gmail.com>
2022-07-06 16:14:31 +0800

    list_fixed

--- include/list.h
+++ include/list.h
@@ -16,7 +16,7 @@ typedef struct list_head {
  * 即返回p的成员node的地址，因p为0地址，表现为从0地址开始算偏移量
  */
 /**
- * @description:
+ * @description:给出结构体首地址
  * @param ptr：所求结构体中list_head成员的地址
  * @param offsetof(member)：求list_head成员距离结构体的偏移量
  * @return
@@ -39,7 +39,7 @@ typedef struct list_head {
  * @description: 从头到尾遍历
  * @return {*}
  */
-#define mylist_for_each(pos, head, type)                                       \
+#define mylist_for_each(pos, head)                                       \
   for (pos = (head)->next; pos != (head); pos = pos->next)
 
 static inline void INIT_MY_LIST_HEAD(mylist_head *list) {
@@ -167,6 +167,7 @@ static inline int mylist_empty(const mylist_head *head) {
   mylist_head *next = head->next;
   return (next == head) && (next == head->prev);
 }
+
 /**
  * @description:
  * 把某一指定节点旋转至链表的最前，使用时应注意node属于head对应的链表
@@ -180,7 +181,6 @@ static inline void mylist_rotate_node_head(mylist_head *node,
     mylist_move_head(node, head);
   }
 }
-
 /**
  * @description:
  * 把某一指定节点旋转至链表的最后，使用时应注意node属于head对应的链表
